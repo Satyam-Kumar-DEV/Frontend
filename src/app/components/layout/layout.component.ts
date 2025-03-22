c20ba1295c83f932a69bfef4a7bfea5fc437c3ca
satyam kumar <satyamku40@gmail.com>
2025-03-22 20:30:55 +0530

    some fixes

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { HomeComponent } from '../home/home.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
-  imports: [HeaderComponent, HomeComponent],
+  imports: [HeaderComponent, HomeComponent, FooterComponent],
 })
 export class LayoutComponent {}
